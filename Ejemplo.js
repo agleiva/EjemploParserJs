e43445c0f0ff4f6de78998ab5fc8f4b51f8d9c84
Agustin <agustinleiva689@gmail.com>
2018-12-22 14:13:24 -0300

    initial commit

---
+++ Ejemplo.js
@@ -0,0 +1,28 @@
+﻿foo = 0;
+final = 0;
+
+if (A != 5 && (A < 10 || A > 20)) {
+    throw SomethingWrongException();
+} else {
+    foo = 15;
+}
+
+if (B != 1 && B != 3 && B != 5 && B != 10 && B != 35) {
+    throw SomethingWrongException();
+} else {
+    if (A > 13) {
+        foo = foo / 2;
+    } else {
+        foo = foo + A;
+    }
+}
+
+if (C > A || C == A && foo < 42) {
+    throw SomethingWrongException();
+} else {
+    if (foo + B == 40) {
+        final = foo - 1;
+    } else {
+        final = foo;
+    }
+}
---
+++ Program.fs
@@ -0,0 +1,42 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open Argu
+open Jint
+
+type CommandLineArguments = | [<MainCommand>] FilePath of path:string
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | FilePath _ -> "ruta al archivo .js que se desea ejecutar"
+
+[<EntryPoint>]
+let main argv =
+    let parser = ArgumentParser.Create<CommandLineArguments>(programName = "JsParser.exe")
+
+    let args = parser.ParseCommandLine argv
+    
+    if not(args.Contains(FilePath)) then
+        Console.Write(parser.PrintUsage())
+        -1
+    else
+        let path = args.GetResult(FilePath)
+        printf "Ejecutando Archivo: %s" path
+        
+        let action f = Action<obj>(f)
+
+        let engine = 
+            Engine().SetValue("SomethingWrongException", action(fun x -> failwith("Something Wrong")))
+
+        engine.SetValue("A", 5)
+
+        let code = File.ReadAllText(path)
+
+        engine.Execute code
+
+        0
+    
+    
+
